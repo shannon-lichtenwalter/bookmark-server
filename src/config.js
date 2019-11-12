9dd5f70d3a93464431a0e0f9ac3064ba511fff31
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-11-12 15:17:28 -0700

    changes to DB_URL

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
-  DB_URL: process.env.DB_URL
+  DB_URL: process.env.DATABASE_URL,
+  TEST_DB_URL: process.env.TEST_DATABASE_URL
 };
